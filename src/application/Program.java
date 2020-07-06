8f8e9ec3b4e26b5ebc7f0cf3889584b3a38237ac
LeniseDeQueirozSilvaLozanoo <lenise.queiroz.silva@gmail.com>
2020-07-06 18:50:18 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,10 @@ public class Program {
 		
 		Department obj = new Department(1, "livro");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(22, "bob", "bob@gmail.com", new Date(), 333.24, obj);
+		
+		System.out.println(seller);
 
 	}
 
